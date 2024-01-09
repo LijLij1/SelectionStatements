d1b414f0c8d17d3d950de33d4b38236e8173a655
Elijah Blair <elijahblair2005@gmail.com>
2024-01-09 10:39:36 -0500

    complete

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -1,37 +1,65 @@
-﻿namespace SelectionStatementExercise
+﻿using Microsoft.VisualBasic;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var r = new Random();
-            var favNumber = r.Next(1, 1000);
-            int userInput;
-            var counter = 0;
+            //var r = new random();
+            //var favnumber = r.next(1, 1000);
+            //int userinput;
+            //var counter = 0;
 
-            do
+            //do
+            //{
+            //console.writeline("pick a number 1 to 1000.");
+            //userinput = int.parse(console.readline());
+
+            //if (userinput < favnumber)
+            //{
+            //console.writeline("to low!");
+            //}
+            // else if (userinput > favnumber)
+            // {
+            //      console.writeline("to high!");
+            // }
+            // else
+            // {
+            //Console.WriteLine("good job you got it!");
+            // }
+
+            // counter++;
+            // console.writeline($"you have tried {counter} times!");
+            //  } while (userinput != favnumber);
+
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
             {
-                Console.WriteLine("Pick a number 1 to 1000.");
-                userInput = int.Parse(Console.ReadLine());
-
-                if (userInput < favNumber)
-                {
-                    Console.WriteLine("To low!");
-                }
-                else if (userInput > favNumber)
-                {
-                    Console.WriteLine("To high!");
-                }
-                else
-                {
-                    Console.WriteLine("Good job you got it!");
-                }
-
-                counter++;
-                Console.WriteLine($"You have tried {counter} times!");
-            } while (userInput != favNumber);
-
-
+                case "math":
+                case "calaculus":
+                    Console.WriteLine("Math is my favorite!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is my second favorite@");
+                    break;
+                case "history":
+                    Console.WriteLine("History is boring!");
+                    break;
+                case "english":
+                case "reading":
+                    Console.WriteLine("English can be fun!");
+                    break;
+                case "p.e":
+                    Console.WriteLine("I love P.E!");
+                    break;
+                default:
+                    Console.WriteLine("I dont know this subject.");
+                    break;
+            }
 
 
 
